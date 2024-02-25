99dd473b8667790482b197f5f430bcf27850d6d5
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-02-25 21:33:50 +0800

    fix: configure omz before load

--- custom/exports.zsh
+++ custom/exports.zsh
@@ -1,23 +1,5 @@
 #!/usr/bin/env zsh
 
-# Path to your oh-my-zsh installation.
-export ZSH=~/.oh-my-zsh
-
-# theme
-export ZSH_THEME="aboqasem"
-
-# Which plugins would you like to load?
-export plugins=(
-  git
-  zsh-syntax-highlighting
-  zsh-autosuggestions
-  you-should-use
-  timer
-)
-
-# `timer` plugin
-export TIMER_FORMAT="[%d]"
-
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
 export PYTHONIOENCODING='UTF-8'
 
@@ -48,20 +30,3 @@ export GPG_TTY=$(tty)
 
 # Do not auto update Homebrew
 export HOMEBREW_NO_AUTO_UPDATE=1
-
-# Uncomment the following line to disable auto-setting terminal title.
-export DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
-export COMPLETION_WAITING_DOTS="true"
-
-# Uncomment the following line if pasting URLs and other text is messed up.
-# export DISABLE_MAGIC_FUNCTIONS="true"
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-export DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Load oh-my-zsh
-source "$ZSH/oh-my-zsh.sh"
--- synced/symlink/.zshrc
+++ synced/symlink/.zshrc
@@ -1,3 +1,18 @@
+# theme
+export ZSH_THEME="aboqasem"
+
+# Which plugins would you like to load?
+export plugins=(
+  git
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+  you-should-use
+  timer
+)
+
+# `timer` plugin
+export TIMER_FORMAT="[%d]"
+
 # Path to your dotfiles.
 export DOTFILES="$HOME/dev/dotfiles"
 
@@ -6,3 +21,20 @@ export ZSH="$HOME/.oh-my-zsh"
 
 # Path to your custom oh-my-zsh dir.
 export ZSH_CUSTOM="$DOTFILES/custom"
+
+# Uncomment the following line to disable auto-setting terminal title.
+export DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+export COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# export DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+export DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Load oh-my-zsh
+source "$ZSH/oh-my-zsh.sh"
