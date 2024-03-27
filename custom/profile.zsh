10ad6812cc9e9e722b7273c9d51a8be612376014
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-03-27 15:18:52 +0800

    fix: enable atuin in Warp to keep record history

--- custom/profile.zsh
+++ custom/profile.zsh
@@ -10,7 +10,7 @@ if type brew &>/dev/null; then
 fi
 
 # atuin init
-[[ $TERM_PROGRAM != "WarpTerminal" ]] && eval "$(atuin init zsh --disable-up-arrow)"
+eval "$(atuin init zsh --disable-up-arrow)"
 
 # bun completions
 [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
